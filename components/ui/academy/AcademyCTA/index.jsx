9b6d5725932fe5784b215fc6aa0f53101183b163
Gai Isay <gaiisayloutlook.de>
2023-07-18 15:34:10 +0200

    finish academy

--- components/ui/academy/AcademyCTA/index.jsx
+++ components/ui/academy/AcademyCTA/index.jsx
@@ -11,13 +11,32 @@ const FooterCTA = () => {
           </h2>
           <p className="mt-3">Claim your purpose model canvas now!</p>
         </div>
-        <div className="flex-none mt-4 md:mt-0">
+        <div className="flex flex-wrap justify-center gap-4 mt-4 md:mt-0">
           <NavLink
             href="https://calendly.com/manaen/workshop"
             target="_blank"
-            className="inline-flex items-center mx-16 text-sm font-medium text-white gap-x-2 bg-logo"
+            className="inline-flex items-center mx-8 text-sm font-medium text-white gap-x-2 bg-logo"
           >
             Get started
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </NavLink>
+          <NavLink
+            href="https://www.mypurpose.academy/"
+            target="_blank"
+            className="inline-flex items-center mx-8 text-sm font-medium text-white gap-x-2 bg-logo"
+          >
+            Learn more{" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
--- components/ui/standard/Navbar/index.jsx
+++ components/ui/standard/Navbar/index.jsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import NavHeader from "../NavHeader";
 import NavLink from "../NavLink";
 import { useRouter } from "next/router";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 const Navbar = () => {
   const { pathname } = useRouter();
   const [state, setState] = useState(false);
   const menuBtnEl = useRef();
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   const navigation = [
     { name: "Home", href: "/" },
     { name: "About", href: "/about" },
-    { name: "Academy", href: "/academy" },
+    { name: "Tools", children: [{ name: "Academy", href: "/academy" }] },
   ];
 
   useEffect(() => {
@@ -47,6 +53,57 @@ const Navbar = () => {
           >
             <ul className="items-center justify-center space-y-6 md:flex md:space-x-6 md:space-y-0">
               {navigation.map((item, idx) => {
+                if (item.children) {
+                  return (
+                    <Menu
+                      key={idx}
+                      as="div"
+                      className="relative inline-block text-left"
+                    >
+                      <div>
+                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md py-2 text-sm ">
+                          Tools
+                          <ChevronDownIcon
+                            className="w-5 h-5 -mr-1 text-gray-400"
+                            aria-hidden="true"
+                          />
+                        </Menu.Button>
+                      </div>
+
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg dark:bg-teal-900 focus:outline-none">
+                          <div className="py-1">
+                            {item.children.map((item, idx) => (
+                              <Menu.Item key={idx}>
+                                {({ active }) => (
+                                  <a
+                                    href={item.href}
+                                    className={classNames(
+                                      active
+                                        ? "dark:bg-teal-800 bg-slate-200"
+                                        : "",
+                                      "block px-4 py-2 text-sm"
+                                    )}
+                                  >
+                                    {item.name}
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            ))}
+                          </div>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  );
+                }
                 return (
                   <li
                     key={idx}
